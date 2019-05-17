ebe0f618e659fee25fbad43542291d7e5af9c3e3
Joe Cummings <jrcummings27@gmail.com>
2019-05-17 17:55:45 -0500

    more tests and additional language elems

--- main.rkt
+++ main.rkt
@@ -2,17 +2,33 @@
 (require redex)
 
 (define-language tglc
-  (e ::= x v (λ (f x) e) (e e) (+ e e) (ref e) (! e) (:= e e))
+  (e ::= x v (fun f (x) e) (e e) (+ e e) (ref e) (! e) (:= e e)) ; expr (incomplete)
   (x f ::= variable-not-otherwise-mentioned)
-  (a ::= integer) ;address
-  (v ::= a integer) 
-  (E ::= hole (E e) (v E) (+ E e) (+ v E) (ref E) (! E) (:= E e))
+  (a ::= integer) ; address
+  (σ ::= · (\|-> a h)) ; heaps (incomplete)
+  (h ::= (λ (x) e) v) ; heap values
+  (β ::= · (\|-> a h)) ; blame sets (incomplete)
+  (b ::= L) ; blame elems (incomplete)
+  (L ::= *) ; labelled types (incomplete)
+  (v ::= a integer) ; values
+  (E ::= hole (E e) (v E) (+ E e) (+ v E) (ref E) (! E) (:= E e)) ; E (incomplete)
   #:binding-forms
-  (λ (f x) e #:refers-to x))
+  (λ (f x) e #:refers-to x)) ;; not sure if this is correct
 (default-language tglc)
 
-(test-equal (redex-match? tglc e (term y)) #t)
-(test-equal (redex-match? tglc e (term (λ (coolfunction y) (y 1)))) #t)
-(test-equal (redex-match? tglc e (term (+ 2 1))) #t)
+(module+ test
+  (test-equal (redex-match? tglc e (term y)) #t)
+  (test-equal (redex-match? tglc e (term 123)) #t)
+  (test-equal (redex-match? tglc e (term (fun coolfunction (y) y))) #t)
+  (test-equal (redex-match? tglc e (term (x x))) #t)
+  (test-equal (redex-match? tglc e (term (ref x))) #t)
+  (test-equal (redex-match? tglc e (term (! x))) #t)
+  (test-equal (redex-match? tglc e (term (:= x y))) #t)
+  (test-equal (redex-match? tglc e (term (+ 2 1))) #t))
 
-(test-results)
+(module+ test
+  (test-equal (redex-match? tglc a 123123123) #t)
+  (test-equal (redex-match? tglc v 123123) #t))
+
+(module+ test
+  (test-results))
