93b17001c72997f9cd09f8c33b336d44a66b0b80
Joe Cummings <jrcummings27@gmail.com>
2019-05-20 14:21:27 -0500

    first try at deref relation

--- main.rkt
+++ main.rkt
@@ -1,22 +1,38 @@
 #lang racket
 (require redex)
 
+(define count 0)
+
 (define-language tglc
   (e ::= x v (fun f (x) e) (e e) (+ e e) (ref e) (! e) (:= e e)) ; expr (incomplete)
   (x f ::= variable-not-otherwise-mentioned)
-  (a ::= integer) ; address
-  (σ ::= · (↦ a h)) ; heaps (incomplete)
+  (a ::= natural) ; address
+  (σ ::= · ⟨(↦ a h) σ⟩) ; heaps (incomplete)
   (h ::= (λ (x) e) v) ; heap values
-  (β ::= · (↦ a h)) ; blame sets (incomplete)
-  (b ::= ⟨a r⟩ L) ; blame elems (incomplete)
+  (β ::= · ⟨(↦ a b) β⟩) ; blame sets (incomplete)
+  (b ::= ⟨a r⟩ L) ; blame elems
   (T ::= int (→ T T) * (ref T)) ; types
-  (L ::= int∈ (→q L L) (refq L) * ⊥l) ; labelled types 
-  (v ::= a integer) ; values
+  (L ::= intq (→q L L) (refq L) * ⊥l) ; labelled types 
+  (v ::= a natural) ; values
   (E ::= hole (E e) (v E) (+ E e) (+ v E) (ref E) (! E) (:= E e) (⇒ ) ; E (incomplete)
   #:binding-forms
   (λ (f x) e #:refers-to x))) ;; not sure if this is correct
 
 (default-language tglc)
+
+(define-metafunction tglc
+  \delta : σ a -> h or \bottom)
+  [(\delta σ a (if (< a count) h \bottom))] ; metafunction for mapsto
+
+(define-judgement-form tglc
+  #:mode (→ I O)
+  #:contract (→ ⟨e σ β⟩ ⟨e σ β⟩) ; one state -> different state
+
+  [( where v_answer (\delta σ a) ((\delta σ a) v)
+    -------------- "deref"
+    (→ ⟨e σ β⟩ ⟨v_answer σ β⟩))]
+
+  )
 
 (module+ test
   (test-equal (redex-match? tglc e (term y)) #t)
