17d7bef62d155d87b83bbe752b7a5d7e5e0f8313
Joe Cummings <jrcummings27@gmail.com>
2019-05-22 15:39:24 -0500

    + judgment and metafunction

--- main.rkt
+++ main.rkt
@@ -12,7 +12,8 @@
   (b ::= (a r) L) ; blame elems
   (T ::= int (→ T T) * (ref T)) ; types
   (L ::= intq (→q L L) (refq L) * ⊥l) ; labelled types 
-  (v ::= a natural) ; values
+  (v ::= a n) ; values
+  (n ::= natural) ; naturals
   (E ::= hole (E e) (v E) (+ E e) (+ v E) (ref E) (! E) (:= E e) (⇒ ) ; E (incomplete)
      #:binding-forms
      (λ (f x) e #:refers-to x))) ;; not sure if this is correct
@@ -29,18 +30,31 @@
 (check-exn exn:fail? (λ () (term (lookup ((0 (λ (x) x)) (1 27) ·) 0))) "not a value")
 (check-exn exn:fail? (λ () (term (lookup ((0 (λ (x) x)) (1 27) ·) 3))) "not found")
 
+(define-metafunction tglc
+  plus : n_1 n_2 -> n
+  [(plus n_1 n_2) ,(+ (term n_1) (term n_2))])
+
+(test-equal (term (plus 1 2)) 3)
+(test-equal (term (plus 0 0)) 0)
+
 (define-judgment-form tglc
   #:mode (→ I O)
   #:contract (→ (e σ) (e σ)) ; one state -> different state
 
   [(where v_answer (lookup σ a))
-   -------------------------------"deref"
+   -----------------------------"deref"
    (→ ((! a) σ) (v_answer σ))]
+
+  [(where n_prime (plus n_1 n_2))
+   -------------------------------- "add"
+   (→ ((+ n_1 n_2) σ) (n_prime σ))]
 
   )
 
 (test-judgment-holds
    (→ ((! 1) ((0 (λ (x) x)) (1 27) ·)) (27 ((0 (λ (x) x)) (1 27) ·))))
+(test-judgment-holds
+   (→ ((+ 1 2) ·) (3 ·)))
 
 
 
